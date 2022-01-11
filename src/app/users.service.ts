a6a2c33ace42290b30fbee576fb832ccdc57f330
chad svastisalee <swasgrafx@gmail.com>
2022-01-11 12:00:43 -0600

    Generate User Edit

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -35,4 +35,8 @@ export class UsersService {
   getUser(id:String): Observable<User>{
     return this.http.get<User>(`${this.url}/${id}`, httpOptions);  
   }
+  updateUser(user: User): Observable<User>{
+    return this.http.put<User>(this.url, user, httpOptions);
+  }
+
 }
